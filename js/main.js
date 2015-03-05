7b143e3e53ab1d9d02856a56e4cdee772ebf3dcf
chenise-paquette <paqu0309@algonquinlive.com>
2015-03-05 14:59:50 -0500

    Expanded and Collapse
    3rd excersise

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var $btnShowHide = $('.btn-show-hide');
+var $box = $('.box');
+
+var $btnMove = $('.btn-move');
+var $diamond = $('.diamond');
+
+var $btnCollapseExpand = $('.btn-collapse-expand');
+var $panel = $('.panel')
+
+
+$btnCollapseExpand.on('click', function () {
+   $panel.toggleClass('js-panel-expand'); 
+});
