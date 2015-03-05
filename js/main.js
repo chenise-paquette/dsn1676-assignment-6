d5ff069c9d6f3269913952fc038aed51bc201b14
chenise-paquette <paqu0309@algonquinlive.com>
2015-03-05 15:15:02 -0500

    Show and hide
    excersie 1

--- js/main.js
+++ js/main.js
@@ -11,3 +11,11 @@ var $panel = $('.panel')
 $btnCollapseExpand.on('click', function () {
    $panel.toggleClass('js-panel-expand'); 
 });
+
+$btnShowHide.on('click', function () {
+   $box.toggleClass('js-box-showhide'); 
+});
+
+$btnMove.on('click', function () {
+   $diamond.toggleClass('js-move-in-out'); 
+});
